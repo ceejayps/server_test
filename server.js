a5bf00946fa6f6cb9f373faa4a4b1af3bf0506d0
ceejayps <ceejayps1308@gmail.com>
2023-08-12 20:16:09 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotEnv = require("dotenv").config({ path: 'server.env' });
+const morgan = require("morgan");
+const path = require('path');
+
+// initialize http server
+const server = express();
+
+//set port number
+const PORT = process.env.PORT || 1876;
+
+//log request to server in console
+server.use(morgan("tiny"));
+
+//parse request with body parser
+server.use(express.json());
+// Use helmet middleware to set security-related headers
+server.use(express.static('public'));
+
+
+
+server.get("/",
+ async (req, res) => {
+  const userAgent = req.headers['user-agent'];
+  console.log(userAgent)
+   return res.sendFile(path.join(__dirname + `/website/`));
+})
+
+
+
+
+
+//server event listener
+server.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
